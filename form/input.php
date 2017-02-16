f92841427e1a28ec813be14884126ade4b6ef09e
prince2398 <princepiyush2398@gmail.com>
2017-02-16 16:45:00 +0530

    modified form

--- form/input.php
+++ form/input.php
@@ -1,6 +1,6 @@
 <?php 
-   $fnm_err=$lnm_err=$email_err=$mn_err=$country_err="";
-   $firstname=$lastname=$email=$mobilenumber=$country=$website=$comment=" ";
+   $fnm_err=$lnm_err=$email_err=$mno_err=$country_err=$website_err=$address_err="";
+   $firstname=$lastname=$email=$mobilenumber=$country=$website=$webaddress=$comment=" ";
    if ($_SERVER["REQUEST_METHOD"]=="POST"){
 	   
 	  if (empty($_POST["firstname"])){
@@ -12,10 +12,18 @@
 			$fnm_err="Only letters and white spaces are allowed";
 		}
 	  }
-	  if(empty ($_POST["lastname"]))
+	  
+	  
+	  if(empty ($_POST["lastname"])){
+		  $lnm_err="";
+	  }
 	  else {
 		$lastname=input($_POST["lastname"]);
+		if(!preg_match("/^[a-zA-Z]*$/",$lastname)){
+			$lnm_err="Only letters and white spaces are allowed";
 	  }
+	  
+	  
      if (empty($_POST["email"])){
 	    $email_err="email is required";
      }
@@ -25,23 +33,42 @@
 			$email_err="invalid email format";
 		}
      }
+	 
+	 
    
      if(empty($_POST["mobilenumber")
-        $mn_err=" mobilenumber is required";
+        $mno_err=" mobilenumber is required";
      else{
 		$mobilenumber=input($_POST["mobilenumber"]);
      }
+	 
+	 
      if(empty($_POST["country"])){
 		country_err="country is required";
 	 }
 	 else{
 		$country=input($_POST["country"]);
 	 }
+	 
+	 if (empty($POST["website"])){
+		 $website_err="option required";
+	 }
+	 else {
 		$website=input($_POST["website"]);
-		if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) { 
-		    $website_err = "Invalid URL"; ;
+	    if($website=='Yes'){
+			if(empty($POST["webaddress"])){
+				address_err="Web Address is required";
+			}
+	        else{
+				$webaddress=input($POST["webaddress"]);
+			    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$webaddress)) { 
+	                $address_err = "Invalid URL"; ;
+	            }
+			}
 		}
-		$comment=input($_POST["comment"]);
+	 }
+    
+	$comment=input($_POST["comment"]);
 		
 		
 		
