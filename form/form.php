1dcfba8145bccbcd629ef66c19c3f7c0de1c2ecc
prince2398 <princepiyush2398@gmail.com>
2017-02-17 23:20:09 +0530

    form.php completed

--- form/form.php
+++ form/form.php
@@ -13,7 +13,7 @@
     <img src="contactus1.jpg">
   <?php 
     
-   $fname=$lname=$email=$mnumber=$country=$website=$address=$comment"";
+   $fname=$lname=$email=$mnumber=$country=$website=$address=$comment="";
    $fname_err=$lname_err=$email_err=$mno_err=$country_err=$website_err=$address_err="";
    
    
@@ -70,14 +70,14 @@
        }	  	   
 	   
 	 
-	if(empty($_POST["website"]){
+	if(empty($_POST["website"])){
 	   $website_err="Option is requiresd";
 	}
 	else{
        $website=input($_POST["website"]);
 	   if($website=="yes"){
 		  if(empty($_POST["address"])){
-		      address_err="address is required";
+		      $address_err="address is required";
 		  }
 		  else{
 			 $address=input($_POST["address"]);
@@ -94,7 +94,7 @@
 	  
    
    
-   }
+   
    
   function input($data){
      $data=trim($data);
@@ -104,6 +104,7 @@
 	}
   ?>
  
+ <br>Submitting  on <?php echo date("d-m-y"); ?><br>
  <p> Fields marked <span class='err'>*</span>are mandatory </p>
  
  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" >
@@ -133,11 +134,13 @@
     <select name="country" value="<?php echo $country;?>" >
 	   <option value="">Please Select</option>
      <?php 
+	 
 	 $countries = array("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
-        foreach($countries as country){
-           echo " <option value=".$country.">".$country."</option>";
+        foreach($countries as $count){
+           echo " <option ";if( isset($country) && ($country==$count)) echo "selected"; echo "value=".$count.">".$count."</option>";
 		}
-     ?>		   
+     
+	 ?>		   
 
 	 
     </select>
@@ -146,8 +149,9 @@
 	
 	Do you have website presently ? <span class="err">*</span><br>
 	<select name="website" value="<?php  echo $website;?>">
-	   <option <?php if( isset($country) && ($country=="yes")) echo "selected"; ?> value="yes">Yes</option>
-       <option <?php if( isset($country) && ($country=="no")) echo "selected"; ?> value="no">No</option>	   
+	   <option value="">Please select</option>
+	   <option <?php if( isset($website) && ($website=="yes")) echo "selected"; ?> value="yes">Yes</option>
+       <option <?php if( isset($website) && ($website=="no")) echo "selected"; ?> value="no">No</option>	   
 	
 	</select><br>
 	
